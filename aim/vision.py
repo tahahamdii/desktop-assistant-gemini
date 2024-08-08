bee98934df6f2099c764688fcd0a0c93de110a45
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-08 14:45:23 +0200

    vision2

--- aim/vision.py
+++ aim/vision.py
@@ -1,3 +1,7 @@
 from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from decouple import config
+
+api_key = config("GOOGLE_GEMINI_API_KEY")
+vision_model = ChatGoogleGenerativeAI(model="gemini-pro-vision", google_api_key=api_key)
+screenshot_file = config("MEDIA_DIR") + "/" + config("SCREENSHOT_FILE")
