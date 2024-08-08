c802f7f9c6e9bda029111771cfa4c87df6cf3886
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-08 14:45:40 +0200

    vision img desc

--- aim/vision.py
+++ aim/vision.py
@@ -5,3 +5,19 @@ from decouple import config
 api_key = config("GOOGLE_GEMINI_API_KEY")
 vision_model = ChatGoogleGenerativeAI(model="gemini-pro-vision", google_api_key=api_key)
 screenshot_file = config("MEDIA_DIR") + "/" + config("SCREENSHOT_FILE")
+
+
+def describe_image():
+    message = HumanMessage(
+        content=[
+            {
+                "type": "text",
+                "text": "This is image of a video feed. Describe only what you see in it, and absolutely nothing else."
+            },
+            {"type": "image_url", "image_url": screenshot_file}
+        ]
+    )
+
+    result = vision_model.invoke([message])
+    print(result.content)
+    return result.content
